a47659173eb74b6851e1ced41c4098bade597af5
Denis <gaz91.91@mail.ru>
2022-12-18 14:57:02 +0500

    Автокомплит второго пароля

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,7 +34,7 @@
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
-                            name="password_confirmation" required />
+                            name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
