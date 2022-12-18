7e78a65a7fe580d430ac3e96dc172f2947a0064f
Denis <gaz91.91@mail.ru>
2022-12-18 23:14:14 +0500

    Исправил баг

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,7 +61,7 @@
                                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Карточка сэмпла [Значений: {{ $sample->values->count() }}].</p>
                                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                                                 <strong>Автор:</strong>
-                                                {{ $sample->user->full_name }}
+                                                {{ $sample->user?->full_name }}
                                             </p>
                                             <div class="flex gap-2 flex-wrap">
                                                 <a href="{{ route('samples.show', $sample->id) }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
